9715c55368443999ca45af45bf7574a47b60ed3e
SharmaKumarRohit <134692002+SharmaKumarRohit@users.noreply.github.com>
2023-05-31 17:49:55 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,58 @@
+'use strict';
+// console.log("output"); --> ya content ouput print karega
+console.log("Hello World!");
+// console.log("Hello World!");
+// console.log("Hello Rohit!");
+// console.log("B C Collage");
+
+// There are three types of define variables:-
+// var
+// let
+// const
+
+// variable rule:-
+// i) no smbole no special characters
+// ii) no spaces no start any number
+// iii) only use for underscore( _ ) symbole
+
+// what is var?
+
+var Myname = 'Rohit Kumar Sharma';
+console.log(Myname);
+
+// var maei multiple times same varible name ko use kar shakte hai.
+// aur ak bar define karne ka baad fir baad maei v use kar shate hai.
+
+var num;
+num = 42;
+console.log(num+4);
+
+// what is let?
+
+let myName = "Rohit";
+console.log(myName);
+
+// let maei ak bar maei ak varible name ko he define kar shakte hai.
+// aur ak bar define karne ka baad fir baad maei v use kar shakte hai.
+
+let takeName;
+takeName = "Rohit";
+console.log(takeName);
+
+// what is const?
+
+const pi = 3.14;
+console.log(pi);
+
+// const ko decleare and define const maei he karna hota.
+// nahi to error throw karega.
+// aur const ko decleare and define ak shath karna hota hai.
+
+const JsNum = "Abbay Sharma";
+console.log(JsNum);
+
+// js bena decleare kiya v varibales baana deta hai. so, asha na esliya top par write down karete hai ke 'use strict'; ka use karte hai.
+
+let name = "Rohit Kumar";
+firstname = "rohit";
+console.log(name);
